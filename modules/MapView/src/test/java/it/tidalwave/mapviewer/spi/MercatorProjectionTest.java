5275f9b062cd114804c92fdfc0418a93e0f594ac
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2025-01-08 20:36:19 +0100

    Added tests.

--- modules/MapView/src/test/java/it/tidalwave/mapviewer/spi/MercatorProjectionTest.java
+++ modules/MapView/src/test/java/it/tidalwave/mapviewer/spi/MercatorProjectionTest.java
@@ -10,18 +10,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MercatorProjectionTest
   {
     @Test(dataProvider = "coordinates")
-    public void test (final int tileSize, final int zoom, final MapCoordinates coordinates, final MapPoint mapPoint)
+    public void test_coordinatesToMapPoint (final int tileSize, final int zoom, final MapCoordinates coordinates, final MapPoint point)
       {
+        // given
         final var underTest = new MercatorProjection(tileSize);
-        final var point = underTest.coordinatesToMapPoint(coordinates, zoom);
-        assertThat(distance(point, mapPoint)).isLessThan(1);
+        // when
+        final var result = underTest.coordinatesToMapPoint(coordinates, zoom);
+        // then
+        assertThat(distance(result, point)).isLessThan(1E-2);
       }
 
+    /**********************************************************************************************************************************************************/
+    @Test(dataProvider = "coordinates")
+    public void test_maPointToCoordinates (final int tileSize, final int zoom, final MapCoordinates coordinates, final MapPoint point)
+      {
+        // given
+        final var underTest = new MercatorProjection(tileSize);
+        // when
+        final var result = underTest.mapPointToCoordinates(point, zoom);
+        // then
+        assertThat(distance(result, coordinates)).isLessThan(1E-2);
+      }
+
+    /**********************************************************************************************************************************************************/
     @DataProvider
     public Object[][] coordinates()
       {
         // +------------------+------------------+
-        // |[0,57]            |          [512,57]|
+        // |[0,57]            |          [511,57]|
         // |                  |                  |
         // |                  |                  |
         // |                  |                  |
@@ -36,14 +52,14 @@ public class MercatorProjectionTest
 
         return new Object[][]
           {
-            {256, 1, MapCoordinates.of(0, 0), MapPoint.of(256.000000, 256.000000) },
-            {256, 1, MapCoordinates.of(45, 45), MapPoint.of(320.000000, 184.179219) },
-            {256, 1, MapCoordinates.of(80, 0), MapPoint.of(256.000000, 57.476812) },
-            {256, 1, MapCoordinates.of(80, 180), MapPoint.of(512.000000, 57.476812) },
-            {256, 1, MapCoordinates.of(0, 180), MapPoint.of(512.000000, 256.000000) },
-            {256, 1, MapCoordinates.of(0, -180), MapPoint.of(0.000000, 256.000000) },
-            {256, 1, MapCoordinates.of(80, -180), MapPoint.of(0.000000, 57.476812) },
-            {256, 1, MapCoordinates.of(-80, -180), MapPoint.of(0.000000, 454.523188) },
+            {256, 1, MapCoordinates.of(  0,    0), MapPoint.of(256.000000, 256.000000) },
+            {256, 1, MapCoordinates.of( 45,   45), MapPoint.of(320.000000, 184.179219) },
+            {256, 1, MapCoordinates.of( 80,    0), MapPoint.of(256.000000,  57.476812) },
+            {256, 1, MapCoordinates.of( 80,  180), MapPoint.of(512.000000,  57.476812) },
+            {256, 1, MapCoordinates.of(  0,  180), MapPoint.of(512.000000, 256.000000) },
+            {256, 1, MapCoordinates.of(  0, -180), MapPoint.of(  0.000000, 256.000000) },
+            {256, 1, MapCoordinates.of( 80, -180), MapPoint.of(  0.000000,  57.476812) },
+            {256, 1, MapCoordinates.of(-80, -180), MapPoint.of(  0.000000, 454.523188) },
           };
       }
   }

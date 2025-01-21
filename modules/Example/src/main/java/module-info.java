e9535bdc742a4dcef8de8d0a4e5b6d755d1190bd
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2025-01-21 12:41:21 +0100

    MV-20: Added Java 9 modules support.

---
+++ modules/Example/src/main/java/module-info.java
@@ -0,0 +1,38 @@
+/*
+ * *************************************************************************************************************************************************************
+ *
+ * MapView: a JavaFX map renderer for tile-based servers
+ * http://tidalwave.it/projects/mapview
+ *
+ * Copyright (C) 2024 - 2025 by Tidalwave s.a.s. (http://tidalwave.it)
+ *
+ * *************************************************************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ * *************************************************************************************************************************************************************
+ *
+ * git clone https://bitbucket.org/tidalwave/mapview-src
+ * git clone https://github.com/tidalwave-it/mapview-src
+ *
+ * *************************************************************************************************************************************************************
+ */
+module it.tidalwave.mapview.example
+  {
+    requires static lombok;
+    requires static jsr305;
+    requires org.slf4j;
+    requires jakarta.annotation;
+    requires it.tidalwave.mapview;
+    requires io.jenetics.jpx;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    opens it.tidalwave.mapview.javafx.example to javafx.graphics, javafx.fxml;
+  }
